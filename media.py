c2154e1d3ed6e5fff0ff4f01c13369bd9fa423c5
Ricky Biven <rbiven@gmail.com>
2017-03-20 09:12:57 -0500

    style: making code PEP8 approved.

--- media.py
+++ media.py
@@ -8,23 +8,25 @@ Building a class for a movie trailer webpage
 """
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie related information.
 The class Movie has the following instance variables:
-       -Title 
-       -Storyline 
-       -Poster Image 
-       -Trailer 
+       -Title
+       -Storyline
+       -Poster Image
+       -Trailer
     """
-    
-    #VALID_RATINGS = ["G", "PG", "PG-13", "R"]    
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    # VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-     
+
     # show_trailor will open a trailer URL when the movie poster is selected.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
